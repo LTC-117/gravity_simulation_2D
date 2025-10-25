38a0ab663c04bca7c022e5336a8a27846cebd854
LTC-117 <lucastentoni@gmail.com>
2025-10-24 21:10:05 -0300

    [Minor formatting adjustments]

--- src/gravity.c
+++ src/gravity.c
@@ -7,7 +7,8 @@ const double G_CONST = (6.67430e-2);
 const double PI = 3.1415926;
 
 
-cel_body define_planet(double radius, double vel_x, double vel_y, double x_pos, double y_pos) {
+cel_body define_planet(double radius, double vel_x, double vel_y, double x_pos, double y_pos)
+{
     srand(time(NULL));
 
     cel_body planet = {
@@ -23,7 +24,8 @@ cel_body define_planet(double radius, double vel_x, double vel_y, double x_pos, 
 }
 
 
-void update_planet_pos(cel_body *p1, cel_body *p2, double delta_t) {
+void update_planet_pos(cel_body *p1, cel_body *p2, double delta_t)
+{
     // distâncias x e y servem apenas para contextualização vetorial
     double dist_x = p1->x - p2->x;
     double dist_y = p1->y - p2->y;
@@ -49,7 +51,8 @@ void update_planet_pos(cel_body *p1, cel_body *p2, double delta_t) {
 }
 
 
-void fill_planet(SDL_Surface *surface, cel_body planet, Uint32 color){
+void fill_planet(SDL_Surface *surface, cel_body planet, Uint32 color)
+{
     double low_x = planet.x - planet.radius;
     double low_y = planet.y - planet.radius;
     double high_x = planet.x + planet.radius;
@@ -57,11 +60,11 @@ void fill_planet(SDL_Surface *surface, cel_body planet, Uint32 color){
 
     double radius_squared = planet.radius * planet.radius;
 
-    for(double x=low_x; x < high_x; x++){
-        for(double y=low_y; y < high_y; y++){
+    for (double x=low_x; x < high_x; x++) {
+        for (double y=low_y; y < high_y; y++) {
             //Is coordinate within planet?
             double center_distance = (x-planet.x)*(x-planet.x) + (y-planet.y)*(y-planet.y);
-            if(center_distance < radius_squared){
+            if (center_distance < radius_squared) {
                 SDL_Rect pixel = (SDL_Rect) {x,y,1,1};
                 SDL_FillRect(surface, &pixel, color);
             }
